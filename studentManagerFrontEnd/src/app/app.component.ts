e48ecace18ba6ca5ed095552c66cdcb0a3173373
kirilanastasoff <kirilanastasoff@gmail.com>
2023-09-06 13:08:01 +0300

    fixes frontend

--- studentManagerFrontEnd/src/app/app.component.ts
+++ studentManagerFrontEnd/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Student } from './student';
 import { StudentService } from './student.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +15,7 @@ export class AppComponent implements OnInit {
   constructor(private studentService: StudentService){
 
   }
-  
+
   ngOnInit(): void {
     this.getStudents();
   }
@@ -29,5 +30,95 @@ export class AppComponent implements OnInit {
       }
     )
   }
-  // title = 'studentManagerFrontEnd';
+
+
+  public onAddStudent(addForm: NgForm): void {
+    document.getElementById('add-student-form')!.click();
+    this.studentService.addStudent(addForm.value).subscribe(
+      (response: Student) => {
+        console.log(response);
+        this.getStudents();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      }
+    );
+  }
+
+
+
+
+  
+
+
+  public onOpenModal(student: Student, mode: string): void{
+    const container = document.getElementById('main-container')!;
+
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal')
+
+    if(mode === 'add'){
+      button.setAttribute('data-target', '#addStudentModal')
+    }
+    if(mode === 'edit'){
+      button.setAttribute('data-target', '#editStudentModal')
+    }
+    if(mode === 'delete'){
+      button.setAttribute('data-target', '#deleteStudentModal')
+    }
+
+    container.appendChild(button);
+    button.click();
+
+  }
+
+
+  public onUpdateEmloyee(student: Student): void {
+    this.studentService.updateStudent(student).subscribe(
+      (response: Student) => {
+        console.log(response);
+        this.getStudents();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onDeleteEmloyee(studentId: number): void {
+    this.studentService.deleteStudent(studentId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getStudents();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public searchStudents(key: string): void {
+    console.log(key);
+    const results: Student[] = [];
+    for (const student of this.students) {
+      if (student.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || student.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || student.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || student.studentTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+        results.push(student);
+      }
+    }
+    this.students = results;
+    if (results.length === 0 || !key) {
+      this.getStudents();
+    }
+  }
+
+
+
+
 }
