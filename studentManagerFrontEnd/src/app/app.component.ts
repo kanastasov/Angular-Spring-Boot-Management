c59ce5906809655999d73e73690e948e6c7a1d6a
kirilanastasoff <kirilanastasoff@gmail.com>
2023-09-06 10:55:20 +0300

    front end fixes

--- studentManagerFrontEnd/src/app/app.component.ts
+++ studentManagerFrontEnd/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Student } from './student';
+import { StudentService } from './student.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'studentManagerFrontEnd';
+export class AppComponent implements OnInit {
+  public students: Student[] = [];
+
+  constructor(private studentService: StudentService){
+
+  }
+  
+  ngOnInit(): void {
+    this.getStudents();
+  }
+
+  public getStudents(): void{
+    this.studentService.getStudents().subscribe(
+      (response: Student[]) => {
+        this.students = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    )
+  }
+  // title = 'studentManagerFrontEnd';
 }
--- studentManagerFrontEnd/src/app/student.service.ts
+++ studentManagerFrontEnd/src/app/student.service.ts
@@ -2,13 +2,14 @@ import { Observable } from "rxjs/internal/Observable";
 import { Student } from "./student";
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from "./environments/environment";
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class StudentService{
-    private apiServerUrl = '';
+    private apiServerUrl = environment.apiBaseUrl
 
     constructor(private http: HttpClient){}
 
